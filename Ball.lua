046072dfed833acc56fcf54d4121a46177779121
Yipeng Zhou <yipeng6@ualberta.ca>
2019-05-10 13:51:21 -0600

    added collision

--- Ball.lua
+++ Ball.lua
@@ -6,16 +6,26 @@ function Ball:init(x,y,width,height)
   self.width = width
   self.height = height
 
-  self.dx = math.random(2) == 1 and 300 or -300
-  self.dy = math.random(-100, 100)
+  self.dy = math.random(2) == 1 and 150 or -150
+  self.dx = math.random(2) == 1 and math.random(-240, -300) or math.random(240, 300)
 
+end
+
+function Ball:collides(paddle)
+  if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+    return false
+  end
+  if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+    return false
+  end
+  return true
 end
 
 function Ball:reset()
   self.x = WINDOW_WIDTH/2 -5
   self.y = WINDOW_HEIGHT/2 -5
   self.dx = math.random(2) == 1 and 300 or -300
-  self.dy = math.random(-100, 100)
+  self.dy = math.random(-150, 150)
 end
 
 function Ball:update(dt)
